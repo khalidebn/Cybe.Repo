82bdc06ac301171235a3eed5cc2d20aee7b797ae
KDB <124183922+khalidebn@users.noreply.github.com>
2024-11-02 21:52:22 +0300

    change the value of variable

--- HackTools/Network_Tools/mac_changer.py
+++ HackTools/Network_Tools/mac_changer.py
@@ -2,8 +2,8 @@
 
 import subprocess
 
-interface = "wlan0"
-new_mac = "00:11:22:33:44:55"
+interface = input("Type your interface: ")
+new_mac = input("Type a new MAC address: ")
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 

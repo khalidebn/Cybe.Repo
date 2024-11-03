d2fec631dc0ba35ee517e52660d826baabd4c760
KDB <124183922+khalidebn@users.noreply.github.com>
2024-11-03 16:24:01 +0300

    Update mac_changer.py

--- HackTools/Network_Tools/mac_changer.py
+++ HackTools/Network_Tools/mac_changer.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 
 import subprocess
+import argparse
 
-interface = input("Type your interface >> ")
-new_mac = input("Type a new MAC address >> ")
 
-print("[+] Changing MAC address for " + interface + " to " + new_mac)
+def change_mac(interface, new_mac):
+    print(f"[+] Changing MAC address for {interface} to {new_mac}")
 
-subprocess.call("ifconfig " + interface + " down ", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up ", shell=True)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+parser = argparse.ArgumentParser(
+    description="Change MAC address of a network interface")
+parser.add_argument("-i", "--interface", dest="interface",
+                    required=True, help="Type interface to change its MAC address")
+parser.add_argument("-m", "--mac", dest="new_mac",
+                    required=True, help="Type new MAC address")
+
+args = parser.parse_args()
+
+change_mac(args.interface, args.new_mac)

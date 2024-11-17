e3be08d60b558dc1557aeaf779bcbd1a5ac92ec5
KDB <124183922+khalidebn@users.noreply.github.com>
2024-11-17 16:37:07 +0400

    Create packet_sniffer.py

---
+++ HackTools/PacketSniffer/packet_sniffer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+from scapy.layers import http
+
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False,
+                prn=process_sniffed_packet, filter="udp")
+
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        print(packet)
+
+
+sniff("eth0")

